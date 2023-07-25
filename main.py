efe78fcf2cc10a675fa88209db3f47a7d8351b03
rapidduck <robin.skaba@volny.cz>
2023-07-25 20:20:24 +0200

    minor

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 from core import substitution
 
-# template_path = input("Template path: ")
-# substitute_path = input("Substitute path: ")
-
-template_path = "meme3.jpg"
-substitute_path = "buff_tree.jpg"
+template_path = input("Template path: ")
+substitute_path = input("Substitute path: ")
 
 result = substitution(template_path, substitute_path)
 
