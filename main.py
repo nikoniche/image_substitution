425eff24bc316e2698e1f780ede880d5b15a4de5
rapidduck <robin.skaba@volny.cz>
2023-07-25 21:01:45 +0200

    minor

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+import time
+
 from core import substitution
+from PIL import Image
 
 template_path = input("Template path: ")
-substitute_path = input("Substitute path: ")
+Image.open(template_path).show()
 
+substitute_path = input("Substitute path: ")
+Image.open(substitute_path).show()
+
+print("Generating..")
 result = substitution(template_path, substitute_path)
 
 try:
