70f20bb9342d655257b48416c5deedc1e7a981c1
rapidduck <robin.skaba@volny.cz>
2023-07-25 20:18:55 +0200

    Initial commit

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from core import substitution
 
-template_path = input("Template path: ")
-substitute_path = input("Substitute path: ")
+# template_path = input("Template path: ")
+# substitute_path = input("Substitute path: ")
+
+template_path = "meme3.jpg"
+substitute_path = "buff_tree.jpg"
 
 result = substitution(template_path, substitute_path)
 
